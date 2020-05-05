87ddb06d1ed103b69d6eec7b4b859ca5ad4f3a16
unknown <p.daulby@hotmail.com>
2020-05-05 20:00:41 +0100

    add current cards and card drawing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './App.css';
+import Cards from './Cards.js'
 
 function App() {
-  let card = { 
-    action : "Change",
-    reversed: {
-      action: "Stasis"
-    }
-  }
-  card = random_boolean() ? card : reverseCard(card);
+  let cards = Cards();
+  cards = shuffle(cards);
+  const [num, setNum] = useState(cards.length - 1);
   return (
     <div className="App">
       <header className="App-header">
-        <Card card={card}></Card>
+        <Card card={random_boolean() ? cards[num] : reverseCard(cards[num])}></Card>
+        {
+          num > 2 
+          ? <button onClick={() => {setNum(num-1)}}>Next Card</button>
+          : <button onClick={() => {cards = shuffle(cards); setNum(cards.length-1)}}>Shuffle</button>
+        }
       </header>
     </div>
   );
@@ -23,9 +25,9 @@ function Card(props) {
   <div className="card-border">
     <div className="card-inner">
       <div className="text-box backwards">{props.card.reversed.action}</div>
-      <div className="text-box backwards">{props.card.reversed.social}</div>
+      <div className={props.card.reversed.social != null ? "social-box backwards" : "text-box backwards"}>{props.card.reversed.social}</div>
       <div className ="art-box"/>
-      <div className="text-box">{props.card.social}</div>
+      <div className={props.card.reversed.social != null ? "social-box backwards" : "text-box backwards"}>{props.card.social}</div>
       <div className="text-box">{props.card.action}</div>
     </div>
   </div>);
@@ -43,5 +45,24 @@ const reverseCard = (card) => {
 }
 
 const random_boolean = () => Math.random() >= 0.5;
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 export default App;
---
+++ src/Cards.js
@@ -0,0 +1,122 @@
+export const Cards = () => [
+    { 
+        action : "Change",
+        reversed: {
+          action: "Stasis"
+        }
+    },
+    { 
+        action : "Recognising the Larger Problem",
+        reversed: {
+          action: "Underestimating the Challenge"
+        }
+    },
+    { 
+        action : "Understanding Mysteries",
+        reversed: {
+          action: "Impracticality"
+        }
+    },
+    { 
+        action : "Energy",
+        reversed: {
+          action: "Exhaustion"
+        }
+    },
+    { 
+        action : "Bravery",
+        reversed: {
+          action: "Cowardice"
+        }
+    },
+    { 
+        action : "Simplicity Prevails",
+        reversed: {
+          action: "Creativity Shines"
+        }
+    },
+    { 
+        action : "Great Effort",
+        reversed: {
+          action: "Effort Misspent"
+        }
+    },
+    { 
+        action : "Recognising Opportunity",
+        reversed: {
+          action: "Failing to see Opportunity"
+        }
+    },
+    { 
+        action : "Unforseen Circumstances",
+        reversed: {
+          action: "Stroke of Luck"
+        }
+    },
+    { 
+        action : "Thoughtlessness",
+        reversed: {
+          action: "Achieving the Unexpected"
+        }
+    },
+    { 
+        action : "Ceasing Fruitless Labor",
+        reversed: {
+          action: "Fruitless Labor"
+        }
+    },
+    { 
+        action : "Innovation",
+        reversed: {
+          action: "Arrogance"
+        }
+    },
+    { 
+        action : "Recognising Limits & Dangers",
+        reversed: {
+          action: "Protections Turn Dangerous"
+        }
+    },
+    { 
+        action : "Hope in Bleaknesss",
+        reversed: {
+          action: "Smart Idea turn Foolish"
+        }
+    },
+    { 
+        action : "Productivity",
+        reversed: {
+          action: "Overconfidence"
+        }
+    },
+    { 
+        action : "Moderation",
+        reversed: {
+          action: "Blind Fury"
+        }
+    },
+    { 
+        action : "Cunning",
+        reversed: {
+          action: "Overreaching"
+        }
+    },
+    { 
+        action : "Using the Environment",
+        social : "Duty, Obidence",
+        reversed: {
+          action: "Environmental Hazard",
+          social : "Corruption",
+        }
+    },
+    { 
+        action : "Devastation",
+        social : "Maturity",
+        reversed: {
+          action: "Outmanouvered",
+          social : "Tyranny",
+        }
+    },
+]
+
+export default Cards;
