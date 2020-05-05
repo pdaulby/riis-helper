555706b21872ea553b5039cc492abdcc1da026b0
unknown <p.daulby@hotmail.com>
2020-05-05 19:14:46 +0100

    create initial card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let card = { 
+    action : "Change",
+    reversed: {
+      action: "Stasis"
+    }
+  }
+  card = random_boolean() ? card : reverseCard(card);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card card={card}></Card>
       </header>
     </div>
   );
 }
+
+function Card(props) {
+  return (
+  <div className="card-border">
+    <div className="card-inner">
+      <div className="text-box backwards">{props.card.reversed.action}</div>
+      <div className="text-box backwards">{props.card.reversed.social}</div>
+      <div className ="art-box"/>
+      <div className="text-box">{props.card.social}</div>
+      <div className="text-box">{props.card.action}</div>
+    </div>
+  </div>);
+}
+
+const reverseCard = (card) => {
+  return {
+    action: card.reversed.action,
+    social: card.reversed.social,
+    reversed: {
+      action: card.action,
+      social: card.social
+    }
+  }
+}
+
+const random_boolean = () => Math.random() >= 0.5;
 
 export default App;
