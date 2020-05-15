c2c8248a68775eb6435db75535b439bfd1ca5e5a
unknown <p.daulby@hotmail.com>
2020-05-15 16:48:25 +0100

    add unfinished card redraw logo and break padding

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import redraw from './card-draw.svg';
 
 function Card(props) {
     return (
-      <div className={props.small ? "card-border-small" : "card-border-large"}>
+      <div className={props.small ? "card-small" : "card-large"}>
+      <img src={redraw} className="cornered"/>
+      <div className={"card-border"}>
         <div className="card-inner">
           <div className="text-box backwards">{props.card.reversed.action}</div>
           <div className={props.card.reversed.social != null ? "social-box backwards" : "text-box"}>{props.card.reversed.social}</div>
@@ -11,6 +14,7 @@ function Card(props) {
           <div className={props.card.social != null ? "social-box" : "text-box"}>{props.card.social}</div>
           <div className="text-box">{props.card.action}</div>
         </div>
+      </div>
       </div>);
   }
 
