66fdf6f19a535e7093e2888352eaf72f3553d46d
unknown <p.daulby@hotmail.com>
2020-05-18 11:49:17 +0100

    borders

--- src/jackstuff/Board.js
+++ src/jackstuff/Board.js
@@ -4,28 +4,34 @@ import Deck from './Deck';
 
 function Board(props) {
   let [hand, setHand] = useState([]);
-  let [deck, setDeck] = useState(new Deck("test", [1,2,3,4]))
+  let [deck, ] = useState(new Deck("test", [1,2,3,4]))
   let addToHand = (item) => setHand(hand.concat(item));
   return (
-    <div className="columns table">
-      <div className="rows">
-        <div className="columns decks">
+    <div className="rows table">
+      <div className="columns">
+        decks
+        <div className="rows decks border">
           <Library deck={deck} addToHand={addToHand}/>
         </div>
-        <div className="columns board">
+        <div className="rows board border">
           board
         </div>
       </div>
-      <div className="columns hand">
+      <div className="rows hand border">
+        hand
         {hand}
       </div>
     </div>);
 }
 
-function Library({addToHand, deck}){
-  return (<div className="deck"
+function Library({addToHand, deck}) {
+  return (<div className="deck border"
     onClick={() => {addToHand(deck.draw())}} 
-    >{deck.getName()}</div>);
+>{deck.getName()} {deck.cardsRemaining()}</div>);
+}
+
+function Hand({hand}) {
+
 }
 
 export default Board;
