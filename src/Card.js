fa1ab26e6d34015ad862045b5def954074312259
unknown <p.daulby@hotmail.com>
2020-05-18 08:55:53 +0100

    make corner image in both corners

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,8 @@ import redraw from './card-draw.svg';
 function Card(props) {
     return (
       <div className={props.small ? "card-small" : "card-large"}>
-      {!props.flip && <img src={redraw} className="cornered" alt="draw card if advantage/disadvantage"/>}
+      {!props.flip && <img src={redraw} className="left-img" alt="draw card if advantage/disadvantage"/>}
+      {!props.flip && <img src={redraw} className="right-img" alt="draw card if advantage/disadvantage"/>}
       <div className={"card-border"}>
         <div className="card-inner">
           <div className="text-box backwards">{props.card.reversed.action}</div>
@@ -15,7 +16,8 @@ function Card(props) {
           <div className="text-box">{props.card.action}</div>
         </div>
       </div>
-      {props.flip && <img src={redraw} className="flip-cornered" alt="draw card if advantage/disadvantage"/>}
+      {props.flip && <img src={redraw} className="left-img-flip" alt="draw card if advantage/disadvantage"/>}
+      {props.flip && <img src={redraw} className="right-img-flip" alt="draw card if advantage/disadvantage"/>}
       </div>);
   }
 
