90e2ced28a4d6855a0cafa2e9726e47039c6a06b
unknown <p.daulby@hotmail.com>
2020-05-18 16:37:38 +0100

    add other decks

--- src/jackstuff/Board.js
+++ src/jackstuff/Board.js
@@ -4,17 +4,27 @@ import Deck from './Deck';
 
 function Board(props) {
   let [hand, setHand] = useState([]);
-  let [enemyDeck,] = useState(new Deck("Enemy Deck", "./Enemy/enemy_", 61))
-  let [armourDeck,] = useState(new Deck("Armour Deck", "./Armour/Armourjpg_", 120))
   let addToHand = (item) => item && setHand(hand.concat(item));
   let removeFromHand = (value) => setHand(hand.filter(item => item !== value));
+
+  let [enemyDeck,] = useState(new Deck("Enemy Deck", "./Enemy/enemy_", 61));
+  let [costOfWarDeck,] = useState(new Deck("Cost of War Deck", "./CostOfWar/costofwar_", 61));
+  let [armourDeck,] = useState(new Deck("Armour Deck", "./Armour/Armourjpg_", 120));
+  let [itemDeck,] = useState(new Deck("Item Deck", "./Item/item_", 69));
+  let [locationDeck,] = useState(new Deck("Location Deck", "./Location/location_", 69));
+  let [weaponDeck,] = useState(new Deck("Weapon Deck", "./Weapon/weapon_", 60));
+  
   return (
     <div className="rows table">
       <div className="columns">
         decks
         <div className="rows decks border">
           <Library deck={enemyDeck} addToHand={addToHand} />
+          <Library deck={costOfWarDeck} addToHand={addToHand} />
           <Library deck={armourDeck} addToHand={addToHand} />
+          <Library deck={itemDeck} addToHand={addToHand} />
+          <Library deck={locationDeck} addToHand={addToHand} />
+          <Library deck={weaponDeck} addToHand={addToHand} />
         </div>
         hand
         <div className="rows hand border">
